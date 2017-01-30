a358c6c9f0a0c3a7b352a8b50656a4b802120c50
Andrew Berger <andberger@berkeley.edu>
2017-01-29 20:47:12 -0500

    be more verbose in training

--- bachnet/trainer.py
+++ bachnet/trainer.py
@@ -15,16 +15,19 @@ def train(n_seconds=30, batch_size=10, epoch_size=1000, n_epochs=1000):
     """
     n_time_samples = SAMPLE_RATE * n_seconds
 
+    print("Building model")
+
     model = Model(n_time_samples)
 
     batch_generator = musicnet_generator(n_time_samples,  batch_size=batch_size,
                                          epoch_size=epoch_size, n_epochs=n_epochs)
 
+    print("Beginning training!")
     # epochs only in name, for now
     for train_idx, (raw_wav, discr_wav, metadata) in enumerate(batch_generator):
         st_time = time.time()
         loss = model._train(raw_wav, discr_wav)
         end_time = time.time()
 
-        if train_idx % epoch_size:
+        if train_idx % epoch_size == 0:
             print("Achieved loss of {}. Training took {}".format(loss, end_time - st_time))
